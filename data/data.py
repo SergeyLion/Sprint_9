6db1ff0f744c50427b5f937ee5fb684679bac077
Sergey Lvov <Lvov.Sergey.V@yandex.ru>
2025-04-10 10:51:08 +0300

    Тест запуска паплайна при пуше в репу

--- data/data.py
+++ data/data.py
@@ -6,7 +6,7 @@ class BaseData:
     TIMEOUT = 10  # Тайм-аут ожидания элемента страницы в секундах
 
 class AuthData:
-    URL_AUTH = "https://foodgram-frontend-1.prakticum-team.ru" #Ссылка для тестирования страницы авторизации
+    URL_AUTH = "https://foodgram-frontend-1.prakticum-team.ru" #Ссылка на страницу авторизации
     AUTH_USERNAME = "sergey_lvov@gmail.com" #Логин для авторизации
     AUTH_PASSWORD = "snegoksnegok" #Пароль для авторизации
 
